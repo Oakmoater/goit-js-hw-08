8e7b45fd5b360ba1d6cc0bf00ce4259ecdc2b524
Oakmoater <dubrovecva@gmail.com>
2023-03-12 20:22:36 +0100

    job is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+const currentTimeKey = 'videoplayer-current-time';
+
+const onTimeUpdate = throttle((data) => {
+  const currentTime = data.seconds;
+  localStorage.setItem(currentTimeKey, currentTime);
+}, 1000); 
+
+player.on('timeupdate', onTimeUpdate);
+
+const storedTime = localStorage.getItem(currentTimeKey);
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailArea = document.querySelector('[name="email"]');
+const messageArea = document.querySelector('[name="message"]');
+const storageKey = 'feedback-form-state';
+const formValues = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(storageKey);
+    console.log(formValues);
+}
+
+function onFormInput(event) {
+    formValues.email = emailArea.value;
+    formValues.message = messageArea.value;
+    localStorage.setItem(storageKey, JSON.stringify(formValues));
+}
+
+updateFormValues();
+
+function updateFormValues() {
+    const savedValues = JSON.parse(localStorage.getItem(storageKey));
+    if (savedValues) {
+        emailArea.value = savedValues.email;
+        messageArea.value = savedValues.message;
+    }
+}
